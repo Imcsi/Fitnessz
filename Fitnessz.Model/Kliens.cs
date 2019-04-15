e1890e9d9ab7647ba0a1869923f7ac305dd5ae6e
Imola <imola.demeter@yahoo.com>
2019-04-15 22:13:58 +0300

    Adatbazis kapcsolat kezdese

---
+++ Fitnessz.Model/Kliens.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitnessz.Model
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Kliens
+    {
+        [Key]
+        public int KliensId { get; set; }
+
+        public string Nev { get; set; }
+
+        public  string SzuletesiDatum { get; set; }
+        
+        public string TelSzam { get; set; }
+
+        public string VonalKod { get; set; }
+
+        public string  Nem { get; set; }
+
+
+    }
+}
